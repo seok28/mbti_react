7c5a8faf51d2d75159058fba91c7ea032fa85c29
junsu <seokjoonsoo@gmail.com>
2022-05-24 00:39:58 +0900

    routes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,17 +2,24 @@ import React from 'react';
 import styled from 'styled-components';
 import PangImage from '../assets/junsu1.jpg';
 import Button from 'react-bootstrap/Button';
+import { useNavigate } from 'react-router-dom';
 const Home = () => {
-return (
-    <Wrapper>
-    <Header>테스트 중</Header>
-    <Contents>
-    <Title> 제목이 들어갈 곳 </Title>
-    <LogoImage>
-        <img src={PangImage} className="rounded-circle" width ={350} height ={350}></img>
+    const navigate = useNavigate();
+
+
+    const handleClickButton = () => {
+        navigate('/question');
+    }
+    return (
+        <Wrapper>
+            <Header>테스트 중</Header>
+            <Contents>
+                <Title> 제목이 들어갈 곳 </Title>
+                <LogoImage>
+                    <img src={PangImage} className="rounded-circle" width ={350} height ={350}></img>
     </LogoImage>
     <Desc>MBTI를 검사하는 웹페이지를 REACT를 이용하여 만들예정</Desc>
-    <Button> MBTI 검사 시작하기</Button>
+    <Button style={{fontFamily :"SimKyungha"}} onClick ={handleClickButton}> MBTI 검사 시작하기</Button>
     </Contents>
     </Wrapper>
 )
@@ -30,6 +37,7 @@ const Header = styled.div `
  display:flex;
  justify-content:center;
  align-items:center;
+ font-family:"SimKyungha";
 `
 const Contents = styled.div `
 display:flex;
@@ -42,6 +50,7 @@ flex-direction: column;
 const Title = styled.div `
  font-size :30pt;
  margin-top: 40px;
+ font-family:"SimKyungha";
 
 `
 const LogoImage = styled.div `
@@ -52,4 +61,5 @@ const LogoImage = styled.div `
 const Desc = styled.div`
  font-size: 20pt;
  margin-top: 50px;
+ font-family:"SimKyungha";
 `
